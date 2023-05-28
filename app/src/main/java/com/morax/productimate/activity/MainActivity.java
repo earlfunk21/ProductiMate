759d871f9f2ff5cae6c848d4f4ad4c93f88d5b5e
earlfunk21 <60378076+earlfunk21@users.noreply.github.com>
2023-05-28 21:46:19 +0800

    adding splash screen and colors

--- app/src/main/java/com/morax/productimate/activity/MainActivity.java
+++ app/src/main/java/com/morax/productimate/activity/MainActivity.java
@@ -2,7 +2,9 @@ package com.morax.productimate.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.morax.productimate.R;
 
@@ -12,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent intent = new Intent(MainActivity.this , AccountActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+
     }
 }
